12c9b80fee519bbfad871e53b90fad1e9f3d2dfa
Samarawickrama G.U.N <89275902+GaveshaniSam@users.noreply.github.com>
2024-01-08 00:07:22 +0530

    Add files via upload

---
+++ tests/InvalidLogin.spec.js
@@ -0,0 +1,27 @@
+const {test,expect} =require("@playwright/test")
+
+//Test Case - Attempt to log in with a valid username but an incorrect password
+test("Attempt to log in with a valid username but an incorrect password",async function({page}){
+
+    await page.goto("https://onlinelibrary.wiley.com/")
+
+    //Go to Login Pop-up
+    await page.click("//span[@class='sign-in-label']");
+
+   // Enter User name
+  const usernamePlaceholder = "Enter your email";
+  await page.fill(`[placeholder="${usernamePlaceholder}"]`, "gaveshaniupekshanikethani@gmail.com"); 
+
+  // Enter Password
+  const passwordPlaceholder = "Enter your password";
+  await page.fill(`[placeholder="${passwordPlaceholder}"]`, "######");
+  
+  //Click on login button
+  await page.locator("//input[@name='submitButton']").click()
+
+  // Add a wait after clicking the login button
+  await page.waitForTimeout(6000); 
+
+
+
+})
---
+++ tests/TabTitle.spec.js
@@ -0,0 +1,23 @@
+const {test,expect} =require("@playwright/test")
+const { title } = require('process')
+
+//Test Case - Verify Wiley Online Library Website Title on the Tab
+
+test("Verify Wiley Online Library Website Title on the Tab",async function({page}){
+
+
+    // Navigate to the Website
+    await page.goto("https://onlinelibrary.wiley.com")
+
+    const url=await page.url()
+
+    console.log("Title is "+url)
+
+    console.log("Title is "+title)
+
+    await expect(page).toHaveTitle("Wiley Online Library | Scientific research articles, journals, books, and reference works")
+
+    // Add a wait 
+    await page.waitForTimeout(6000);
+
+})
